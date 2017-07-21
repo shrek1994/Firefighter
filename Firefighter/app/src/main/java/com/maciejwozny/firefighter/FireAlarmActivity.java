54b4369c6b98ce95b25be79ee4e0b9253467bff7
shrek1994 <maciej.wozny41@gmail.com>
2017-07-21 21:03:09 +0200

    after receice sms, alarm playing

--- Firefighter/app/src/main/java/com/maciejwozny/firefighter/FireAlarmActivity.java
+++ Firefighter/app/src/main/java/com/maciejwozny/firefighter/FireAlarmActivity.java
@@ -2,17 +2,28 @@ package com.maciejwozny.firefighter;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class FireAlarmActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire_alarm);
+        playAlarm(this);
         showAlert(this);
+    }
+
+    private void playAlarm(Context context) {
+        mediaPlayer = MediaPlayer.create(context, R.raw.fire);
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
     }
 
 
@@ -25,7 +36,8 @@ public class FireAlarmActivity extends AppCompatActivity {
                 R.string.yes,
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
+                        mediaPlayer.stop();
+                        dialog.cancel();    
                     }
                 });
 
@@ -33,6 +45,7 @@ public class FireAlarmActivity extends AppCompatActivity {
                 R.string.no,
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
+                        mediaPlayer.stop();
                         dialog.cancel();
                     }
                 });
